ac5ed103e3212245425498fe59f82b8604732630
Matthew Bernheimer <Krutonslol@gmail.com>
2017-08-26 22:41:03 -0400

    send email on successful appointment creation

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from server.app import create_app
+from server.settings import ProdConfig
+import config
 
-app = create_app()
+
+if config.DEBUG:
+    app = create_app()
+else:
+    app = create_app(ProdConfig)
 
 if __name__ == '__main__':
     app.run()
